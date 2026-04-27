9200497e36da82d9701a79311b3ac0c7c26d5265
mariamparedes <mariamparedes@gmail.com>
2026-04-27 23:12:08 +0200

    15: Crear clase Salida para encapsular presentación

---
+++ entregas/FernandaParedes/src/Salida.java
@@ -0,0 +1,8 @@
+public class Salida {
+    public Salida() {
+    }
+
+    public void mostrarGanador(Caballo caballo) {
+        System.out.println("Ganó el caballo " + caballo.obtenerNumero());
+    }
+}
