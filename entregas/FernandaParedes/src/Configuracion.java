562805b2a2e0babfb3df780ad767195a37f224c1
mariamparedes <mariamparedes@gmail.com>
2026-04-27 23:13:36 +0200

    17: Crear clase Configuracion para centralizar constantes

---
+++ entregas/FernandaParedes/src/Configuracion.java
@@ -0,0 +1,12 @@
+public class Configuracion {
+    public static final int PISTA_FINAL = 40;
+    public static final int CANTIDAD_CABALLOS = 2;
+    public static final int AVANCE_MINIMO = 0;
+    public static final int AVANCE_MAXIMO = 2;
+    public static final String SEPARADOR = "----------------------------------------------";
+    public static final String CABALLO_REPRESENTACION = ";-;'";
+
+    private Configuracion() {
+        // Clase de configuración, no debe instanciarse
+    }
+}
